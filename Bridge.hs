b09998d76154b90320e4f7f90d7f19ff5e5b4106
Bartosz Milewski <bartosz@relisoft.com>
2017-12-28 16:27:57 +0100

    Update Bridge.hs
    Prepared for a blog post

--- Bridge.hs
+++ Bridge.hs
@@ -7,41 +7,46 @@ import Data.List
 import qualified Data.Map as Map
 import Data.Maybe
 
-type Part = (Int, Int)
+main = do
+  input <- readFile "Data.txt"
+  let pool = fmap toPiece (lines input)
+  print $ bestChain pool
 
-type Bridge = [Part]
+type Piece = (Int, Int)
 
-type Parts = Map.Map Int [Int]
+type Chain = [Piece]
 
-addPart :: Part -> Parts -> Parts
-addPart (m, n) = if m /= n 
-                 then add m n . add n m
-                 else add m n
+type Pool = Map.Map Int [Int]
+
+addPiece :: Piece -> Pool -> Pool
+addPiece (m, n) = if m /= n 
+                  then add m n . add n m
+                  else add m n
   where 
-    add m n box = 
-      case Map.lookup m box of
-        Nothing -> Map.insert m [n] box
-        Just lst -> Map.insert m (n : lst) box
+    add m n pool = 
+      case Map.lookup m pool of
+        Nothing  -> Map.insert m [n] pool
+        Just lst -> Map.insert m (n : lst) pool
 
-removePart :: Part -> Parts -> Parts
-removePart (m, n) = if m /= n
-                    then rem m n . rem n m
-                    else rem m n
+removePiece :: Piece -> Pool -> Pool
+removePiece (m, n) = if m /= n
+                     then rem m n . rem n m
+                     else rem m n
   where
-    rem :: Int -> Int -> Parts -> Parts
-    rem m n box = 
-      case fromJust $ Map.lookup m box of
-        []  -> Map.delete m box
-        lst -> Map.insert m (delete n lst) box
+    rem :: Int -> Int -> Pool -> Pool
+    rem m n pool = 
+      case fromJust $ Map.lookup m pool of
+        []  -> Map.delete m pool
+        lst -> Map.insert m (delete n lst) pool
 
-presort :: [Part] -> Parts
-presort = foldl' (flip addPart) Map.empty 
+presort :: [Piece] -> Pool
+presort = foldr addPiece Map.empty 
           
--- Tree of bridges
+-- Tree of chains
 -- Each node contains a port
--- and a list of smaller bridges
+-- and a list of smaller chains
 -- Top node will contain port 0
-data TreeB = Nd Int [TreeB]
+data Rose = NodeR Int [Rose]
   deriving Show
 
 -- Represent the tree as a fixed point of a functor
@@ -57,11 +62,11 @@ type Coalgebra f a = a -> f a
 
 -- This coalgebra builds a tree using anamorphism
 
-coalg :: Coalgebra TreeF (Int, Parts)
-coalg (n, box) = 
-  case Map.lookup n box of
-    Nothing -> NodeF 0 []
-    Just ms -> NodeF n [(m, removePart (m, n) box) | m <- ms]
+coalg :: Coalgebra TreeF (Int, Pool)
+coalg (n, pool) = 
+  case Map.lookup n pool of
+    Nothing -> NodeF n []
+    Just ms -> NodeF n [(m, removePiece (m, n) pool) | m <- ms]
     
 ana :: Functor f => Coalgebra f a -> a -> Fix f
 ana coalg = Fix . fmap (ana coalg) . coalg
@@ -70,17 +75,17 @@ type Algebra f a = f a -> a
 
 -- This algebra is for testing
 
-tAlg :: Algebra TreeF TreeB
-tAlg (NodeF n lst) = Nd n lst
+tAlg :: Algebra TreeF Rose
+tAlg (NodeF n lst) = NodeR n lst
 
--- This algebra turns a tree into a list of bridges
-bridgAlg :: Algebra TreeF (Int, [Bridge])
-bridgAlg (NodeF n []) = (n, [])
-bridgAlg (NodeF n lst) = (n, concat [f (n, m) bs | (m, bs) <- lst])
+-- This algebra turns a tree into a list of chains
+chainAlg :: Algebra TreeF (Int, [Chain])
+chainAlg (NodeF n []) = (n, [])
+chainAlg (NodeF n lst) = (n, concat [push (n, m) bs | (m, bs) <- lst])
   where
-    f :: (Int, Int) -> [Bridge] -> [Bridge]
-    f (n, m) [] = [[(n, m)]]
-    f (n, m) bs = [(n, m) : br | br <- bs]
+    push :: (Int, Int) -> [Chain] -> [Chain]
+    push (n, m) [] = [[(n, m)]]
+    push (n, m) bs = [(n, m) : br | br <- bs]
 
 cata :: Functor f => Algebra f a -> Fix f -> a
 cata alg = alg . fmap (cata alg) . unFix
@@ -88,24 +93,16 @@ cata alg = alg . fmap (cata alg) . unFix
 hylo :: Functor f => Algebra f a -> Coalgebra f b -> b -> a
 hylo f g = f . fmap (hylo f g) . g
 
-score :: Bridge -> Int
+score :: Chain -> Int
 score = sum . fmap score1
   where score1 (m, n) = m + n
 
-buildBridges :: [Part] -> Int
-buildBridges parts =
-  let box = presort parts
-      -- t = ana coalg (0, box)
-      -- (_, bridges) = cata bridgAlg t
-      (_, bridges) = hylo bridgAlg coalg (0, box)
-  in maximum $ fmap score bridges
+bestChain :: [Piece] -> Int
+bestChain pieces =
+  let pool = presort pieces
+      (_, chains) = hylo chainAlg coalg (0, pool)
+  in maximum $ fmap score chains
 
-main = do
-  input <- readFile "Data.txt"
-  let parts = fmap toPart (lines input)
-  print $ buildBridges parts
-
-toPart :: String -> Part
-toPart s = let (a : b : _) = splitOn "/" s
+toPiece :: String -> Piece
+toPiece s = let (a : b : _) = splitOn "/" s
          in (read a, read b)
- 
